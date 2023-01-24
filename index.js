66fe232f96c4088fd171bd076cc5bcdb8daceab8
alaeddinehmida <alaeddinehmidazbot@gmail.com>
2023-01-24 22:44:15 +0100

    secondl

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.get('/guest/:id', (req, res) => {
 
 
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log('Server running on port 3000'));
